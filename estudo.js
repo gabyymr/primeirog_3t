c88eea49f8c06ea2ffaa2fa4f94b4114d9c6180a
gabyymr <106184307+gabyymr@users.noreply.github.com>
2022-09-30 12:40:23 -0300

    30/09

---
+++ estudo.js
@@ -0,0 +1,15 @@
+let tabuada = 2;
+
+function escreva(){
+document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
+document.write(tabuada+" x 2 = "+(tabuada*1)+"<br>");
+document.write(tabuada+" x 3 = "+(tabuada*1)+"<br>");
+document.write(tabuada+" x 4 = "+(tabuada*1)+"<br>");
+document.write(tabuada+" x 5 = "+(tabuada*1)+"<br>");
+document.write(tabuada+" x 6 = "+(tabuada*1)+"<br>");
+document.write(tabuada+" x 7 = "+(tabuada*1)+"<br>");
+document.write(tabuada+" x 8 = "+(tabuada*1)+"<br>");
+document.write(tabuada+" x 9 = "+(tabuada*1)+"<br>");
+document.write(tabuada+" x 10 = "+(tabuada*1)+"<br>");
+}
+
